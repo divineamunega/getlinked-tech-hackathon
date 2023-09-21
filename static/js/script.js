25e0e6f4e53db59bbc6eb86b6bfdccaa217eed73
divineamunega <114052520+divineamunega@users.noreply.github.com>
2023-09-21 09:47:12 +0100

    modified:   index.html 	modified:   static/css/style.css 	new file:   static/img/trophy.png 	modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -15,4 +15,30 @@ const navbar = function () {
 	closeNav.addEventListener("click", toggleNav);
 };
 
+const moveDiv = function (container) {
+	const movableDiv = document.createElement("div");
+	// movableDiv.classList.add("blur-grad");
+
+	movableDiv.style.width = "100px";
+	movableDiv.style.height = "100px";
+	movableDiv.style.backgroundColor = "#903aff";
+	movableDiv.style.position = "fixed";
+	movableDiv.style.left = "50%";
+	movableDiv.style.filter = "blur(50px)";
+	movableDiv.style.transition = "transform .15s ease";
+	movableDiv.style.top = "0";
+	movableDiv.style.left = "0";
+	movableDiv.style.zIndex = "-100";
+	document.body.appendChild(movableDiv);
+
+	window.addEventListener("mousemove", (event) => {
+		const x = event.clientX - 75;
+		const y = event.clientY - 75;
+		// movableDiv.style.top = y + "px";
+		// movableDiv.style.left = x + "px";
+		movableDiv.style.transform = `translate(${x}px, ${y}px)`;
+	});
+};
+
 navbar();
+moveDiv();
